b474801886cf58e091d5a55d9b121a5844a892dc
Jeyaseelan R <jeyaseelan.rajendran@handbuiltapps.com>
2026-06-17 22:49:06 +0530

    Revert "whitelist all"
    This reverts commit 59b41adaa6fe4491f8408b9e58f73a93f30e6848.

--- src/index.js
+++ src/index.js
@@ -2,10 +2,7 @@ import cors from 'cors';
 import express from 'express';
 import cookieParser from 'cookie-parser';
 
-import { 
-    // FRONTEND_URL, 
-    PORT 
-} from './config/env.js';
+import { FRONTEND_URL, PORT } from './config/env.js';
 import { setupSwagger } from './config/swagger.js';
 import connectDB from "./config/db.js";
 import testRoutes from './routes/test.route.js';
@@ -20,7 +17,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    // origin: FRONTEND_URL,
+    origin: FRONTEND_URL,
     credentials: true,
 }));
 
