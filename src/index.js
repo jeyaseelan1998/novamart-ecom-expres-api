e97d074033228049ea3d0d4e9fd0ad7ec5cfda53
Jeyaseelan R <jeyaseelan.rajendran@handbuiltapps.com>
2026-06-18 21:50:21 +0530

    fix encoding

--- src/index.js
+++ src/index.js
@@ -15,6 +15,7 @@ const app = express();
 
 // Middlewares
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({
     origin: FRONTEND_URL,
